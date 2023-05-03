72b75d171851fca6d45ba72b6bf7663ba481d854
Gregory Sanchez <mcgregox@gmail.com>
2023-05-03 15:46:33 -0300

    Making new model and controller Article

---
+++ views/article/_article_item.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var $model \app\models\Article */
+
+?>
+
+<div>
+    <h3><?php echo \yii\helpers\Html::encode($model->title) ?></h3>
+    <div>
+        <?php echo \yii\helpers\Html::encode($model->body)  ?>
+    </div>
+    <hr>
+</div>
